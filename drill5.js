4704618b8f25f21b2f2efb7e74035b21a291cf5d
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-02-11 11:42:37 -0800

    drill 5

---
+++ drill5.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const array = [
+  { 
+    boss: 'Bob',
+    'name': 'person1',
+    'job title': 'job1',
+  },
+  {
+    boss: 'Bob',
+    'name': 'person2',
+    'job title': 'job2',
+  },
+  {
+    boss: 'Bob',
+    'name': 'person3',
+    'job title': 'job3',
+  },
+  {
+    'name': 'Bob',
+    'job title': 'job4',
+  },
+];
+
+array.forEach (
+  function(element) {
+    if(!element['boss']) {
+      console.log(`${element['job title']} ${element['name']} doesn't report to anybody.`);
+    } else {
+    console.log(`${element['job title']} ${element['name']} reports to ${element['boss']}.`);
+    }
+  }
+);
