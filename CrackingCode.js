9c11eddacbeed71f83cdba1d7aee4c3d904f1cc0
Keith Freitag <keithfreitag@gmail.com>
2020-02-11 12:46:59 -0800

    cracking code cypher drill

---
+++ CrackingCode.js
@@ -0,0 +1,37 @@
+
+
+function decode(encrypted) {
+    let words = encrypted.split(' ');
+    let result = '';
+    const cypher = {
+        a: 2,
+        b: 3,
+        c: 4,
+        d: 5,
+    }
+
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let firstLetter = word[0];
+        if (cypher[firstLetter] == undefined) {
+            result += ' ';
+        }
+        else {
+            let index = cypher[firstLetter] - 1;
+            result += word[index];
+        }
+    }
+
+
+
+
+    return result;
+}
+
+function decodeWords(words) {
+    return decode(words)
+    
+}
+
+console.log(decodeWords('craft block argon meter bells brown croon droop'))
